3500399f3d8ba467a97c7d11b21c190f03a78e51
ReferTV <HeCorepomoc@gmail.com>
2021-12-21 14:41:51 +0100

    Update main class

--- src/main/java/pl/refertv/tools/Tools.java
+++ src/main/java/pl/refertv/tools/Tools.java
@@ -8,11 +8,19 @@ public final class Tools extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("Uruchamiam plugin" + this.getDescription().getName() + " wersja " + this.getDescription().getVersion());
+        getLogger().info("\n" +
+                "  ____                               __  __   ____   _____                _      \n" +
+                " / ___|  __ _  _ __ ___    ___  ___ |  \\/  | / ___| |_   _|  ___    ___  | | ___ \n" +
+                "| |  _  / _` || '_ ` _ \\  / _ \\/ __|| |\\/| || |       | |   / _ \\  / _ \\ | |/ __|\n" +
+                "| |_| || (_| || | | | | ||  __/\\__ \\| |  | || |___    | |  | (_) || (_) || |\\__ \\\n" +
+                " \\____| \\__,_||_| |_| |_| \\___||___/|_|  |_| \\____|   |_|   \\___/  \\___/ |_||___/\n" +
+                "                                                        §fby " + this.getDescription().getAuthors() + " §a" + this.getDescription().getVersion());
+        getLogger().info("Plugin z narzędziami dla administratora");
+
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Wyłączam plugin" + this.getDescription().getName() + "" + this.getDescription().getVersion());
     }
 }
