4faef93a5c4389922f9228d2f75d45a75a63519e
ReferTV <HeCorepomoc@gmail.com>
2021-12-21 19:15:23 +0100

    GamemodeCommand

--- src/main/java/pl/refertv/tools/cmds/GamemodeCommand.java
+++ src/main/java/pl/refertv/tools/cmds/GamemodeCommand.java
@@ -10,10 +10,28 @@ import java.util.Objects;
 public class GamemodeCommand extends CommandBase {
 
     @Override
-    protected void onCommand(Player s, Command cmd, String label, String[] args) {
-        Player p = s.getPlayer();
-        p.setGameMode(GameMode.CREATIVE);
-        s.sendTitle("GamesMC", "Gamemode został włączony", 10,30,10);
+    protected void onCommand(Player p, Command cmd, String label, String[] args) {
+        if (p.hasPermission("gamesmc.gamemode")) {
+            if (args.length > 0) {
+                if (args[0].equalsIgnoreCase("0")) {
+                    p.setGameMode(GameMode.SURVIVAL);
+                    p.sendTitle("§8•● §6☆ Games§fMC§e.pl §6☆ §8●•", "§eZmieniłeś tryb gry na przetrwanie", 10, 30, 10);
+
+                } else if (args[0].equalsIgnoreCase("1")) {
+                    p.setGameMode(GameMode.CREATIVE);
+                    p.sendTitle("§8•● §6☆ Games§fMC§e.pl §6☆ §8●•", "§eZmieniłeś tryb gry na kreatywny", 10, 30, 10);
+
+                } else if (args[0].equalsIgnoreCase("2")) {
+                    p.setGameMode(GameMode.ADVENTURE);
+                    p.sendTitle("§8•● §6☆ Games§fMC§e.pl §6☆ §8●•", "§eZmieniłeś tryb gry na przygodowy", 10, 30, 10);
+
+                } else if (args[0].equalsIgnoreCase("3")) {
+                    p.setGameMode(GameMode.SPECTATOR);
+                    p.sendTitle("§8•● §6☆ Games§fMC§e.pl §6☆ §8●•", "§eZmieniłeś tryb gry na obserwatora", 10, 30, 10);
+                }
+            }
+        }
     }
 }
+
 
