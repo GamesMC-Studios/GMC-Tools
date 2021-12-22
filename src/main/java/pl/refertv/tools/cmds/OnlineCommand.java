31e5ac50c9fe56713b6a25c97e9c44dea36bd6f8
ReferTV <HeCorepomoc@gmail.com>
2021-12-22 11:37:19 +0100

    Online command update

--- src/main/java/pl/refertv/tools/cmds/OnlineCommand.java
+++ src/main/java/pl/refertv/tools/cmds/OnlineCommand.java
@@ -14,8 +14,8 @@ public class OnlineCommand extends CommandBase {
     protected void onCommand(Player p, Command cmd, String label, String[] args) {
         if (p.hasPermission("gamesmc.online")) {
             if(cmd.getName().equalsIgnoreCase("online")){
-                p.sendMessage("Aktualnie na serwerze jest " + Bukkit.getOnlinePlayers().size() + "/" + Bukkit.getMaxPlayers());
-                p.sendTitle("§8•● §6☆ Games§fMC§e.pl §6☆ §8●•", "§e" + Bukkit.getOnlinePlayers().size() + "/" + Bukkit.getMaxPlayers(), 10, 50, 10);
+                p.sendMessage("Aktualnie na serwerze jest " + Bukkit.getOnlinePlayers().size() + "/" + Bukkit.getMaxPlayers() + " graczy online");
+                p.sendTitle("§8•● §6☆ Games§fMC§e.pl §6☆ §8●•", "§eAktualnie na serwerze jest: " + Bukkit.getOnlinePlayers().size() + "/" + Bukkit.getMaxPlayers(), 10, 50, 10);
             }
         }
     }
