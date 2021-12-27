ac4ea366b45b6e41392c3bc07f35582227f0536f
ReferTV <HeCorepomoc@gmail.com>
2021-12-27 19:00:53 +0100

    New time arguments: lock, unlock

--- src/main/java/pl/refertv/tools/cmds/TimeCommand.java
+++ src/main/java/pl/refertv/tools/cmds/TimeCommand.java
@@ -1,6 +1,7 @@
 package pl.refertv.tools.cmds;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 import pl.refertv.tools.Tools;
@@ -13,14 +14,18 @@ public class TimeCommand extends CommandBase {
             if (args.length == 0) {
                 p.sendTitle(Tools.gmc, Tools.arg);
                 return true; }
-            {
-            if (args[0].equalsIgnoreCase("dzień") || (args[0].equalsIgnoreCase("day"))) {
+            { if (args[0].equalsIgnoreCase("dzień") || (args[0].equalsIgnoreCase("day"))) {
                 p.getWorld().setTime(6000);
                 Bukkit.getOnlinePlayers().forEach(player -> player.sendTitle(Tools.gmc, " Czas został zmieniony na §edzień §fprzez administratora §6" + p.getName()));
-            }
-            if (args[0].equalsIgnoreCase("noc") || args[0].equalsIgnoreCase("night")) {
+            } if (args[0].equalsIgnoreCase("noc") || args[0].equalsIgnoreCase("night")) {
                 p.getWorld().setTime(18000);
                 Bukkit.getOnlinePlayers().forEach(player -> player.sendTitle(Tools.gmc, "Czas został zmieniony na §8noc §fprzez administratora §6" + p.getName()));
+            } if (args[0].equalsIgnoreCase("zatrzymaj") || (args[0].equalsIgnoreCase("lock"))) {
+                p.getWorld().setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+                Bukkit.getOnlinePlayers().forEach(player -> player.sendTitle(Tools.gmc, "Czas został zatrzymany §fprzez administratora §6" + p.getName()));
+            } if (args[0].equalsIgnoreCase("wznów") || (args[0].equalsIgnoreCase("unlock"))) {
+                p.getWorld().setGameRule(GameRule.DO_DAYLIGHT_CYCLE, true);
+                Bukkit.getOnlinePlayers().forEach(player -> player.sendTitle(Tools.gmc, "Czas został wznowiony §fprzez administratora §6" + p.getName()));
             }
             }
         } else {
